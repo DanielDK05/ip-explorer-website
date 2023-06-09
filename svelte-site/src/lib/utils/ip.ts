cd83a6655b3572f4534a4ad78f976208e6478b83
DanielDK05 <62153701+DanielDK05@users.noreply.github.com>
2023-06-09 14:19:05 +0200

    CLEANUP ON AISLE 69 I REPEAT CLEANUP ON AISLE 69

--- svelte-site/src/lib/utils/ip.ts
+++ svelte-site/src/lib/utils/ip.ts
@@ -1,172 +1,39 @@
-export function validateIpString(ipStr: string, shouldIncludeCIDR: boolean): boolean {
+export function StringToNumericIP(str: string): number | undefined {
 	const ipRegex =
 		/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-	const cidrRegex = /(?:\/(3[0-2]|[12]?[0-9]))?$/;
 
-	const validationRegex = new RegExp(ipRegex.source + (shouldIncludeCIDR ? cidrRegex.source : ''));
+	if (!str.match(ipRegex)) return undefined;
 
-	return validationRegex.test(ipStr);
-}
-
-export function matchIpCIDRWithNetmask(cidr: number, netMask: string) {
-	return createNetmaskFromCIDR(cidr) === netMask;
-}
-
-export function validateNetmaskStr(netMaskStr: string): boolean {
-	if (!validateIpString(netMaskStr, false)) return false;
-
-	const netMaskByteStr = netMaskStr
+	const binStr = str
 		.split('.')
 		.map((octet) => parseInt(octet).toString(2).padStart(8, '0'))
 		.join('');
-	const netmaskRegex = /^(1{1,31}0*)$/;
 
-	return netmaskRegex.test(netMaskByteStr);
+	return parseInt(binStr, 2);
 }
 
-export function ipStrHasCIDR(ipStr: string): boolean {
-	const netMaskMatch = ipStr.match(/\/(3[0-2]|[12]?[0-9])$/);
-
-	return netMaskMatch ? true : false;
+export function getNetworkAddress(ip: number, mask: number): number {
+	return (ip & mask) >>> 0;
 }
 
-export function extractNetmaskFromIpStr(ipStr: string): string | undefined {
-	const netMaskMatch = ipStr.match(/\/(3[0-2]|[12]?[0-9])$/);
-
-	if (!netMaskMatch) return undefined;
-
-	const prefixLength = parseInt(netMaskMatch[1]);
-	const MAX_PREFIX = 32; // 4 bytes
-
-	if (prefixLength > MAX_PREFIX || prefixLength < 0) return undefined;
-
-	let bitStr = '1'.repeat(prefixLength) + '0'.repeat(MAX_PREFIX - prefixLength);
-
-	const octetArr: number[] = [];
-	// loop through every byte
-	for (let i = 0; i < bitStr.length; i += 8) {
-		const byteSubStr = bitStr.substring(i, i + 8);
-
-		octetArr.push(parseInt(byteSubStr, 2));
-	}
-
-	return octetArr.join('.');
+export function getBroadcastAddress(ip: number, mask: number): number {
+	const invertedMask = ~mask >>> 0;
+	const broadcastAddress = ip | invertedMask;
+	return broadcastAddress >>> 0;
 }
 
-export function extractCIDRFromIpStr(ipStr: string): number | undefined {
-	const netMaskMatch = ipStr.match(/\/(3[0-2]|[12]?[0-9])$/);
-
-	return netMaskMatch ? parseInt(netMaskMatch[1]) : undefined;
-}
-
-export function createNetmaskFromCIDR(CIDR: number): string | undefined {
-	const MAX_PREFIX = 32; // 4 bytes
-
-	if (CIDR > MAX_PREFIX || CIDR < 0) return undefined;
-
-	let bitStr = '1'.repeat(CIDR) + '0'.repeat(MAX_PREFIX - CIDR);
-
-	const octetArr: number[] = [];
-	// loop through every byte
-	for (let i = 0; i < bitStr.length; i += 8) {
-		const byteSubStr = bitStr.substring(i, i + 8);
-
-		octetArr.push(parseInt(byteSubStr, 2));
-	}
-
-	return octetArr.join('.');
-}
-
-export function convertIpStrToBinaryString(ipStr: string): string | undefined {
-	if (!validateIpString(ipStr, false)) return undefined;
-
-	return ipStr
-		.split('.')
-		.map((octet) => parseInt(octet).toString(2).padStart(8, '0'))
-		.join('');
-}
-
-export function convertBinaryStringToIpStr(binStr: string): string | undefined {
-	if (binStr.length !== 32) return undefined;
-
-	const octetArr: number[] = [];
-
-	for (let i = 0; i < binStr.length; i += 8) {
-		const byteSubStr = binStr.substring(i, i + 8);
-
-		octetArr.push(parseInt(byteSubStr, 2));
-	}
-
-	const ipStr = octetArr.join('.');
-
-	return validateIpString(ipStr, true) ? ipStr : undefined;
-}
-
-export function getNetworkAddress(ipStr: string, netMask: string): string | undefined {
-	if (!validateIpString(ipStr, false) || !validateIpString(netMask, false)) return undefined;
-
-	const binIpStr = convertIpStrToBinaryString(ipStr);
-	const binNetMask = convertIpStrToBinaryString(netMask);
-
-	if (!binIpStr || !binNetMask) return undefined;
-
-	let binNetworkAddress = '';
-	for (let i = 0; i < 32; i++) {
-		binNetworkAddress += parseInt(binIpStr[i]) * parseInt(binNetMask[i]);
-	}
-	return convertBinaryStringToIpStr(binNetworkAddress);
-}
-
-export function getBroadcastAddress(ipStr: string, netMask: string): string | undefined {
-	if (!validateIpString(ipStr, false) || !validateIpString(netMask, false)) return undefined;
-
-	const binIpStr = convertIpStrToBinaryString(ipStr);
-	const binNetMask = convertIpStrToBinaryString(netMask);
-
-	if (!binIpStr || !binNetMask) return undefined;
-
-	let binNetworkAddress = '';
-	let i = 0;
-	while (binNetMask[i] !== '0') {
-		binNetworkAddress += parseInt(binIpStr[i]) * parseInt(binNetMask[i]);
-		i++;
-	}
-	while (i < 32) {
-		binNetworkAddress += '1';
-		i++;
-	}
-
-	return convertBinaryStringToIpStr(binNetworkAddress);
-}
-
-export function removeCIDRFromIpStr(ipStr: string): string | undefined {
-	const netMaskMatch = ipStr.match(/\/(3[0-2]|[12]?[0-9])$/);
-
-	if (!netMaskMatch) return undefined;
-
-	return ipStr.replace(netMaskMatch[0], '');
-}
-
-export function getIpRange(
-	ipStr: string,
-	netMask: string
-): { start: string; end: string } | undefined {
-	const networkAddress = getNetworkAddress(ipStr, netMask);
-	const broadcastAddress = getBroadcastAddress(ipStr, netMask);
-
-	if (!broadcastAddress || !networkAddress) return undefined;
-
-	const start = networkAddress
-		.split('.')
-		.map((octet, index) => (index === 3 ? parseInt(octet) + 1 : octet))
-		.join('.');
-	const end = broadcastAddress
-		.split('.')
-		.map((octet, index) => (index === 3 ? parseInt(octet) - 1 : octet))
-		.join('.');
-
+export function getIpRange(ip: number, mask: number): { start: number; end: number } {
 	return {
-		start: start,
-		end: end
+		start: getNetworkAddress(ip, mask) + 1,
+		end: getBroadcastAddress(ip, mask) - 1
 	};
+}
+
+export function NumericIPToString(ip: number): string {
+	const octets: number[] = [];
+	for (let i = 24; i >= 0; i -= 8) {
+		const octet = (ip >>> i) & 255;
+		octets.push(octet);
+	}
+	return octets.join('.');
 }
--- svelte-site/src/routes/+page.svelte
+++ svelte-site/src/routes/+page.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
-    import {validateIpString, extractCIDRFromIpStr, ipStrHasCIDR, validateNetmaskStr, matchIpCIDRWithNetmask, extractNetmaskFromIpStr, getNetworkAddress, removeCIDRFromIpStr, getBroadcastAddress, getIpRange} from '../lib/utils/ip';
+	import { NumericIPToString, StringToNumericIP, getNetworkAddress, getBroadcastAddress, getIpRange} from "$lib/utils/ip";
 
-    let ipInput: string = "10.169.131.69/24";
+    let ipInput: string = "10.169.131.69";
     let maskInput: string = "255.255.255.0";
 
-    function onIpInputChange(e: Event) {
-        const netMask = extractNetmaskFromIpStr((e.target as HTMLInputElement).value);
-        if (netMask) maskInput = netMask;
+    let IP: number | undefined;
+  let mask: number | undefined;
+  let networkAddress: string | undefined;
+  let broadcastAddress: string | undefined;
+  let ipRange: {start: string, end: string} | undefined;
+
+  $: {
+    IP = StringToNumericIP(ipInput);
+    mask = StringToNumericIP(maskInput);
+
+    if (IP && mask) {
+      networkAddress =NumericIPToString(getNetworkAddress(IP, mask));
+      broadcastAddress = NumericIPToString(getBroadcastAddress(IP, mask));
+      const numericIpRange = getIpRange(IP, mask);
+      ipRange = {
+        start: NumericIPToString(numericIpRange.start),
+        end: NumericIPToString(numericIpRange.end)
+      };
+
+    } else {
+      networkAddress = undefined;
+      broadcastAddress = undefined;
+      ipRange = undefined;
     }
+  }
 </script>
 
 <div>
     <h1>IP Explorer</h1>
-    <p>{validateIpString(ipInput, true) ? 'Valid IP' : 'Invalid IP'}</p>
-    <p>{validateNetmaskStr(maskInput) ? 'Valid Netmask' : 'Invalid Netmask'}</p>
-    <p>{maskInput && ipStrHasCIDR(ipInput) &&  matchIpCIDRWithNetmask(extractCIDRFromIpStr(ipInput) ?? -1, maskInput) ? 'Netmask and IP matches' : 'Netmask and IP doesnt match'}</p>
     <input 
         type="text" 
-        on:input={onIpInputChange} 
         bind:value={ipInput} 
         placeholder="type IP here:"
     >
     <input type="text" bind:value={maskInput} placeholder={"type mask here:"}>
     <div class="info">
-        <p id="networkAddress">{getNetworkAddress(ipStrHasCIDR(ipInput) ? removeCIDRFromIpStr(ipInput) ?? ipInput : ipInput, maskInput)}</p>
-        <p id="broadcastAddress">{getBroadcastAddress(ipStrHasCIDR(ipInput) ? removeCIDRFromIpStr(ipInput) ?? ipInput : ipInput, maskInput)}</p>
-        <p id="ipRange">{getIpRange(removeCIDRFromIpStr(ipInput) ?? ipInput, maskInput)?.start}-{getIpRange(removeCIDRFromIpStr(ipInput) ?? ipInput, maskInput)?.end}</p>
-        <p id="netWorkMask">{extractNetmaskFromIpStr(ipInput) ? extractNetmaskFromIpStr(ipInput) : maskInput}</p>
+        <p id="ip">IP address: {ipInput}</p>
+        <p id="netWorkMask">Network Mask: {maskInput}</p>
+        <p id="networkAddress">Network address: {networkAddress}</p>
+        <p id="broadcastAddress">Broadcast address: {broadcastAddress}</p>
+        <p id="ipRange">IP range: {ipRange && `${ipRange.start}-${ipRange.end}`}</p>
     </div>
 </div> 
