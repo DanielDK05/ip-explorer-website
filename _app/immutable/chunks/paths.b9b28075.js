cc7144c8b6a8898812b70f95db540b7935cede3c
DanielDK05 <62153701+DanielDK05@users.noreply.github.com>
2023-12-12 13:41:15 +0100

    Fixed deployment issue

--- _app/immutable/chunks/paths.b9b28075.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_bhytsv)==null?void 0:s.base)??"/ip-explorer-website";var e;const a=((e=globalThis.__sveltekit_bhytsv)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/chunks/paths.ebc63390.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1oerrh5)==null?void 0:s.base)??"/ip-explorer-website";var e;const a=((e=globalThis.__sveltekit_1oerrh5)==null?void 0:e.assets)??t;export{a,t as b};
